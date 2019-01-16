5cd071c23862340d296931212c868bf4dd63b0cb
Aliaxey <dcr@gmail.com>
2019-01-16 23:11:25 +0300

    initial commit

---
+++ Lesson1/Assets/Scripts/Sphere1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere1 : MonoBehaviour {
+
+    [SerializeField]
+    float gravity = 9;
+    float startSpeed = 0;
+    float startTime = 0;
+    Vector3 startPoint;
+
+	void Start () {
+        startPoint = transform.position;
+	}
+	
+	void Update () {
+        var time = Time.time - startTime;
+        transform.position = new Vector3(startPoint.x, startPoint.y + (startSpeed * time) - (gravity * Square(time))/2, startPoint.z);
+	}
+    private void OnCollisionEnter(Collision collision) {
+        if (startTime == 0) {
+            startSpeed = gravity * Time.time;
+        }
+        startPoint = transform.position;
+        startTime = Time.time;
+    }
+    private static float Square(float arg) {
+        return arg * arg;
+    }
+}
---
+++ Lesson1/Assets/Scripts/Sphere2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//так я делал на флеше раньше)
+public class Sphere2 : MonoBehaviour {
+   [SerializeField]
+    float gravity = -9;
+    float speed = 0;
+
+	void Start () {
+
+    }
+	
+	void Update () {
+        var dt = Time.deltaTime;
+        speed += gravity * dt;
+        var currentPos = transform.position;
+        currentPos.y += speed * dt;
+        transform.position = currentPos;
+	}
+    private void OnCollisionEnter() {
+        speed = -speed;
+    }
+}
